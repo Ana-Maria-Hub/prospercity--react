13ee81a4617110531343da34aacee1e0ddf2a62b
root <pearpaes@gmail.com>
2019-01-23 12:13:41 -0800

    App.js added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ComponentValida from './components/ComponentValida';
+
+function App() {
+  return (
+    <div className="App">
+      <ComponentValida/>
+    </div>
+  );
+}
+
+export default App;
